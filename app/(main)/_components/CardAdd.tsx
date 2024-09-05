435d3002ceb6e3c29568002f3aa63bbdbaf6c3fe
DanteTerry <sinasa88@gmail.com>
2024-09-05 16:09:07 +0200

    added drag and drop functionality in board page

--- app/(main)/_components/CardAdd.tsx
+++ app/(main)/_components/CardAdd.tsx
@@ -2,28 +2,50 @@
 import { Plus, X } from "lucide-react";
 import { useState } from "react";
 
-function CardAdd() {
+function CardAdd({ getCard }: { getCard: (card: string) => void }) {
   const [card, setCard] = useState("");
   const [show, setShow] = useState(false);
+
+  const saveCard = () => {
+    if (!card) {
+      return;
+    }
+    getCard(card);
+    setCard("");
+    setShow(!show);
+  };
+
+  const closeCard = () => {
+    setCard("");
+    setShow(!show);
+  };
   return (
     <div className="mt-2">
       <div className="flex flex-col">
         {show && (
           <div>
+            {/* text area to set card title */}
             <textarea
               className="rounded-md border-2 bg-zinc-900 p-1"
               name=""
               id=""
+              value={card}
+              onChange={(e) => setCard(e.target.value)}
               cols={24}
               rows={2}
               placeholder="Enter Card Title.."
             ></textarea>
+
+            {/* buttons to save or delete card */}
             <div className="flex p-1">
-              <button className="mr-2 rounded bg-sky-600 p-1 text-white hover:bg-gray-600">
+              <button
+                onClick={saveCard}
+                className="mr-2 rounded bg-sky-600 p-1 text-white hover:bg-gray-600"
+              >
                 Add card
               </button>
               <button
-                onClick={() => setShow(false)}
+                onClick={closeCard}
                 className="rounded p-1 hover:bg-gray-600"
               >
                 <X />
@@ -31,6 +53,8 @@ function CardAdd() {
             </div>
           </div>
         )}
+
+        {/* button to add card */}
         <button
           onClick={() => setShow(true)}
           className="mt-1 flex h-8 w-full items-center justify-start rounded p-1 hover:bg-gray-500"
--- lib/redux/boardSlice.tsx
+++ lib/redux/boardSlice.tsx
@@ -1,44 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export interface BoardSliceType {}
+type ItemType = {
+  id: string;
+  title: string;
+};
+
+type ListType = {
+  id: number;
+  title: string;
+  items: ItemType[];
+};
+
+export type BoardSliceType = {
+  name: string;
+  bgColor: string;
+  list: ListType[];
+};
 
 const initialState: BoardSliceType = {
-  boards: [
+  name: "My Board",
+  bgColor: "#069000",
+  list: [
     {
-      name: "My Board",
-      bgColor: "#069000",
-      list: [
-        {
-          id: 1,
-          title: "To Do",
-          items: [
-            {
-              id: "cdFft",
-              title: "Project description one ",
-            },
-          ],
-        },
-        {
-          id: 2,
-          title: "In progress",
-          items: [
-            {
-              id: "cdFfft",
-              title: "Project description Two ",
-            },
-          ],
-        },
-        {
-          id: 3,
-          title: "Done",
-          items: [
-            {
-              id: "cdFaft",
-              title: "Project description three ",
-            },
-          ],
-        },
-      ],
+      id: 1,
+      title: "To Do",
+      items: [],
+    },
+    {
+      id: 2,
+      title: "In progress",
+      items: [],
+    },
+    {
+      id: 3,
+      title: "Done",
+      items: [],
     },
   ],
 };
@@ -46,11 +42,15 @@ const initialState: BoardSliceType = {
 export const boardSlice = createSlice({
   name: "board",
   initialState,
-  reducers: {},
+  reducers: {
+    setBoard: (state, action) => {
+      return action.payload;
+    },
+  },
 });
 
 // export the actions
-export const {} = boardSlice.actions;
+export const { setBoard } = boardSlice.actions;
 
 // Export the reducer
 export default boardSlice.reducer;
