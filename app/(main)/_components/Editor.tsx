e10627bd0c20770a4a6e39d7c824ea3ec13c6d0a
DanteTerry <sinasa88@gmail.com>
2024-09-30 23:52:30 +0200

    fixed editor issue while publishing

--- app/(main)/_components/Editor.tsx
+++ app/(main)/_components/Editor.tsx
@@ -1,12 +1,11 @@
 "use client";
-
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
-import { useEffect, useState } from "react";
 import { BlockNoteView } from "@blocknote/mantine";
 import { uploadFiles } from "@/utils/uploadthing";
 import { db } from "@/config/firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { sanitizeBlocks } from "@/utils/blockNoteUtil";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 
@@ -23,37 +22,45 @@ function Editor({
   const [documentOutput, setDocumentOutput] = useState<PartialBlock[]>([]);
   const [editor, setEditor] = useState<BlockNoteEditor | null>(null);
 
-  // Fetch the document from Firestore
+  const saveDocument = async (document: PartialBlock[]) => {
+    const docRef = doc(db, "PageDocumentOutput", params?.documentId);
+    await updateDoc(docRef, { output: document });
+  };
+
   const getDocument = async () => {
-    const docRef = doc(db, "PageDocumentOutput", params.documentId);
+    const docRef = doc(db, "PageDocumentOutput", params?.documentId);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       const output = docSnap.data()?.output;
-      if (Array.isArray(output)) {
-        setDocumentOutput(output); // Set the document content
+      if (Array.isArray(output) && output.length > 0) {
+        setDocumentOutput(output);
+      } else {
+        // Set default content if no document is found
+        setDocumentOutput([
+          {
+            id: "",
+            type: "paragraph",
+            props: {
+              textColor: "default",
+              backgroundColor: "default",
+              textAlignment: "left",
+            },
+            content: [],
+            children: [],
+          },
+        ]);
       }
     } else {
       console.log("No such document!");
     }
   };
 
-  // Save document function
-  const saveDocument = async (document: PartialBlock[]) => {
-    const docRef = doc(db, "PageDocumentOutput", params.documentId);
-    await updateDoc(docRef, { output: document });
-  };
-
-  // Fetch the document on component mount
   useEffect(() => {
-    if (params) {
-      getDocument();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    getDocument();
   }, [params]);
 
-  // Initialize the editor when documentOutput is fetched
   useEffect(() => {
-    if (documentOutput.length > 0 && !editor) {
+    if (documentOutput.length > 0) {
       const newEditor = BlockNoteEditor.create({
         initialContent: documentOutput,
         uploadFile: async (file: File) => {
@@ -65,7 +72,7 @@ function Editor({
       });
       setEditor(newEditor);
     }
-  }, [documentOutput, editor]);
+  }, [documentOutput]);
 
   return (
     <div className="mx-auto mt-6 md:w-full md:-translate-x-4 lg:w-3/5">
@@ -73,10 +80,8 @@ function Editor({
         <BlockNoteView
           editor={editor}
           onChange={async () => {
-            if (editable) {
-              const data = await sanitizeBlocks(editor.document);
-              saveDocument(data); // Only save document when in editable mode
-            }
+            const data = await sanitizeBlocks(editor.document);
+            await saveDocument(data);
           }}
           style={{
             width: "100%",
